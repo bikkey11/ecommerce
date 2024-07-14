52d0dbfc2ca3fc0040651516841e04da606d7e7e
bikkey11 <86094528+bikkey11@users.noreply.github.com>
2024-07-14 13:54:58 +0545

    Update db.js

--- server/Utils/db.js
+++ server/Utils/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 const DB_URI = `mongodb+srv://`;
 
  const db = () => {
-    mongoose.connect(DB_URI)
+    mongoose.connect(DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error: "));
     db.once("open", function () {
